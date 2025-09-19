e7658d73d7ad374b3b3bc5cd5b23751c26f53677
sambeal <samantha.beal@dal.ca>
2025-09-19 13:47:07 -0400

    early exploration of FINS dataset

---
+++ Scripts/FINS lake exploration.R
@@ -0,0 +1,53 @@
+################################################################################
+# Exploration of FINS lake data set to choose lakes for Ch. 3 - FW survey of NS
+# to assess the impacts of AIS on natural FW fish biodiversity
+
+# will use the same lake selection parameters as EC Honours
+
+# am assessing lakes firstly on a county basis, then will look per watershed 
+# per county
+
+# FINS data set is an excel file and I have saved the two sheets of interest
+# as separate csvs
+
+# will firstly create a short list of lakes in the appropriate range (≤ 100 ha)
+# then will comb through those to see what species were detected and when
+
+# final output will be a list of lakes with AIS invasion status
+################################################################################
+
+# read in lake data ----
+#NOTE this path will need to be updated once I finish cleaning up my files
+lakes <- read.csv("../../3_FW_survey/NS fishes docs/FINS/FINS 2025-06-05_SBmodified_lakesize.csv", header = TRUE)
+
+
+# subset lakes that are ≤ 100 ha
+smalllakes <- subset(lakes, Surface.Area..ha.<= 100)
+
+
+# read in fish data - NOTE this path will need to be updated once I finish cleaning up my files
+fish <- read.csv("../../3_FW_survey/NS fishes docs/FINS/FINS 2025-06-05_SBmodified_lakefish.csv", header = TRUE)
+
+
+################################################################################
+# Add lake size and species info into a new df
+library(dplyr)
+
+# some lakes have multiple entries from resampling -- keep all of these
+lake_fish <- fish %>%
+  left_join(
+    smalllakes %>%
+      select(County, Site.Code, Name, Primary.Watershed, Secondary.Watershed,
+             Watershed.Code, Assessment.Date, Surface.Area..ha.),
+    by = c("County", "Site.Code", "Name",
+           "Primary.Watershed", "Secondary.Watershed", "Watershed.Code",
+           "Captured.Date" = "Assessment.Date"),
+    relationship = "many-to-many"
+  )
+
+
+
+
+
+
+
